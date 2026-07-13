06a3b7636f061f42d96bbffd76c7ed3c7a0d97e9
Omar Herrera <135260857+OmarHSelles@users.noreply.github.com>
2026-07-13 11:31:05 +0200

    feat: create first SwiftUI interface

--- SwiftLearningJourney/ContentView.swift
+++ SwiftLearningJourney/ContentView.swift
@@ -1,19 +1,28 @@
-//
-//  ContentView.swift
-//  SwiftLearningJourney
-//
-//  Created by OMAR on 8/7/26.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View {// voy a crear una pantalla
+    var body: some View {// a qui dentro se crea la pantalla
+        VStack(spacing: 20) {// pila vertical
+
+            Text("¡Hola Omar! 👋")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Mi viaje para convertirme en desarrollador iOS comienza hoy.")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+
+            Button("Comenzar") {
+                print("Hola OMAR")
+                for number in 1...5 {
+                    print("Paso \(number)")
+                }
+                
+                print("Fin de la funcion")
+                
+            }
+            .buttonStyle(.borderedProminent)
+
         }
         .padding()
     }
